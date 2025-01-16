8d326deca7e16bc026bc564f077ecf9a62fc659f
twgever <tommasoleonardi00@gmail.com>
2025-01-16 15:12:34 +0100

    Added one(1) comment and the requirements.txt

--- agent.py
+++ agent.py
@@ -1,5 +1,7 @@
 from typing import Tuple
 
+#Each agent has a position, defined as a pair of int (coordinates), a color, defined
+# as a triple of int (RGB values), and a direction, defined as a pair of int (coordinates)
 class Agent:
     def __init__(self, pos: Tuple[int, int], color: Tuple[int, int, int], direction: Tuple[int, int]):
         self.pos = pos
