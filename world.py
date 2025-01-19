b1159ebcff3383019038a675713b94e71da77d6d
= <saravalle.1948684@studenti.uniroma1.it>
2025-01-19 12:40:03 +0100

    Add batchified agent, landmark and goal creation

--- world.py
+++ world.py
@@ -13,20 +13,38 @@ class World:
         self.width = config["width"]
         self.num_agents = config["num_agents"]
         self.num_landmarks = config["num_landmarks"]
+        self.num_shapes = config["num_shapes"]
+        self.batch_size = config["batch_size"]
 
         #create all of the agents and put them in a tensor
-        # numAgents x ( dimPosition + dimVelocity + dimGaze + dimColor )
-        self.agents = torch.tensor([self.createAgent() for x in range(self.num_agents)]) 
-        print(self.agents)
+        # shape: (batch_size, num_agents, 7)
+        self.agents = self.create_agents_batch()
+        # shape: (batch_size, num_landmarks, 6)
+        self.landmarks = self.create_landmarks_batch()
+        # shape: (batch_size, num_agents, 2)
+        self.goals = self.assign_goals()
 
     #creates an agent, giving it a random position, a random color, a random gaze and
     # a velocity of 0
-    def createAgent(self):
-        pos = [random.randint(0, self.width), random.randint(0, self.height)]
-        velocity = [0, 0]
-        gaze = [random.randint(0, self.width), random.randint(0, self.height)]
-        color = [random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)]
-        return pos + velocity + gaze + color
+    def create_agents_batch(self):
+        pos = torch.randint(0, self.width, (self.batch_size, self.num_agents, 2))
+        velocity = torch.zeros((self.batch_size, self.num_agents, 2))
+        gaze = torch.randint(0, self.width, (self.batch_size, self.num_agents, 2))
+        color = torch.randint(0, 256, (self.batch_size, self.num_agents, 3))
+        shapes = torch.randint(0, self.num_shapes, (self.batch_size, self.num_agents, 1))
 
-    def get_size(self):
-        return self.size
+        agents = torch.cat((pos, velocity, gaze, color, shapes), dim=2)
+        return agents
+    
+    def create_landmarks_batch(self):
+        pos = torch.randint(0, self.width, (self.batch_size, self.num_landmarks, 2))
+        color = torch.randint(0, 256, (self.batch_size, self.num_landmarks, 3))
+        shapes = torch.randint(0, self.num_shapes, (self.batch_size, self.num_landmarks, 1))
+
+        landmarks = torch.cat((pos, color, shapes), dim=2)
+        return landmarks
+
+    def assign_goals(self):
+        goal_type = torch.randint(0, 3, (self.batch_size, self.num_agents, 1)) # 0 do nothing, 1 look at landmark, 2 move to landmark
+        goal_target = torch.randint(0, self.num_landmarks, (self.batch_size, self.num_agents, 1))
+        return torch.cat((goal_type, goal_target), dim=2)
