Fixed roation calculation, reduced batch size

physical_processor.py
@@ -19,9 +19,9 @@ class PhysicalProcessor(nn.Module):
                 nn.Linear(config["hidden_size"], 256)
             )
 
-    def forward(self, x, rmatrix):
+    def forward(self, x, rmatrix, agentpos):
 
-        x = self.rotate(x, rmatrix)
+        x = self.rotate(x, rmatrix, agentpos)
 
         batch, num, dim = x.shape
         x = x.reshape(batch * num, dim).float()
@@ -31,14 +31,19 @@ class PhysicalProcessor(nn.Module):
         x = x.reshape(batch, num, -1)
         return x
     
-    def rotate(self, x, rmatrix):
-        updated_x = x.clone()
+    def rotate(self, x, rmatrix, agentpos):
+        # x: B x NA x 10
+        #rmatrix: B x 2 x 2
+        #agentpos: B x 2
 
-        updated_x[:, :, :2] = updated_x[:, :, :2] @ rmatrix
-        updated_x[:, :, 4:6] = updated_x[:, :, 4:6] @ rmatrix
+        updated_x = x.clone()
+        agentpos = agentpos.unsqueeze(1)
+        rmatrix = rmatrix.unsqueeze(1)
+
+        updated_x[:, :, :2] = torch.matmul(rmatrix,(updated_x[:, :, :2] - agentpos).unsqueeze(3)).squeeze(3)
+        updated_x[:, :, 4:6] = torch.matmul(rmatrix,(updated_x[:, :, 4:6] -agentpos).unsqueeze(3)).squeeze(3)
 
         x = updated_x
-
         return x
     
         

world.py
@@ -146,8 +146,10 @@ class World(nn.Module):
             all_agents_physical_features = []
             #compute the physical representation rotated
             for i in range(self.num_agents):
-                agent_physical_features = self.physical_processor(self.agents, self.rotation_matrices[:,i])
-                landmark_physical_features = self.physical_processor(self.landmarks, self.rotation_matrices[:,i])
+                #for every agent i in every batch, calculate the physical features of agents and landmarks
+                #using its own rotation matrix and its position for the rotation
+                agent_physical_features = self.physical_processor(self.agents, self.rotation_matrices[:,i], self.agents[:,i,:2])
+                landmark_physical_features = self.physical_processor(self.landmarks, self.rotation_matrices[:,i], self.agents[:,i,:2])
                 physical_features = torch.cat((agent_physical_features, landmark_physical_features), dim=1)
                 physical_features = SoftmaxPooling(dim=1)(physical_features)
 